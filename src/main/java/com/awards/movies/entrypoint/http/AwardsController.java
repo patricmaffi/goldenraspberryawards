07b5d6f3b1f428b38b27710a1fba57adb7a88249
PtcMF <patricmaffi@gmail.com>
2022-12-22 18:45:36 -0300

    configs

--- src/main/java/com/awards/movies/entrypoint/http/AwardsController.java
+++ src/main/java/com/awards/movies/entrypoint/http/AwardsController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
