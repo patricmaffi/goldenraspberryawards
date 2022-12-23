33a75fbd4093cf9e5ba8fffce996d8825a782e9a
PtcMF <patricmaffi@gmail.com>
2022-12-23 10:44:11 -0300

    config validations and handlers

--- src/main/java/com/awards/movies/application/config/LoadMovies.java
+++ src/main/java/com/awards/movies/application/config/LoadMovies.java
@@ -16,6 +16,8 @@ public class LoadMovies {
     AddMoviesByCsvHandler handler;
     @PostConstruct
     public List<Movie> run() throws IOException {
-        return handler.handle(new AddMoviesByCsv("static/movielist.csv"));
+//        return handler.handle(new AddMoviesByCsv("static/movielist.csv"))
+        return null;
     }
+
 }
